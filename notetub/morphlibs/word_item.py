4b37daec38d2c050899ce0c6eccd5b6e976dc517
Сергей <zaswed76@gmail.com>
2019-01-15 12:11:46 +0200

    word item

--- notetub/morphlibs/word_item.py
+++ notetub/morphlibs/word_item.py
@@ -15,6 +15,16 @@ def replace_characters(line, trantab):
     """
     return line.translate(trantab)
 
+class LetterObject:
+    def __init__(self, let):
+        self.let = let
+
+d = {}
+alt_dct = {k: v for k, v in zip("бвгджз", "пфктшс")}
+alt_dct2 = {k: v for k, v in zip("пфктшс", "бвгджз")}
+alt_dct.update(alt_dct2)
+
+
 
 class WordItem:
     def __init__(self, word, lev_ratio=None):
@@ -26,18 +36,30 @@ class WordItem:
 
 
     def compare(self, word):
-        print("{} == {}".format(self.word, word))
+        print("{} == {}".format(self.cons_skeleton, word.cons_skeleton))
+        r = 0
+        for n, (lw, fw) in enumerate(zip(self.cons_skeleton, word.cons_skeleton), start=1):
+            print(lw, fw)
+            if lw == fw:
+                r += 30/n
+            elif lw == alt_dct.get(fw):
+                r += 20/n
+            else:
+                pass
+        return r/100 - 0.28
+
+
 
     def __repr__(self):
         return str(self.word)
 
 
 if __name__ == '__main__':
-    word = "корова"
-    it = ("корова", 0.25)
-    wi = WordItem(*it)
-    print(wi.cons_skeleton)
-    print(wi.cons_deaf_ring_id)
+    word_line = WordItem("повод")
+    find_word1 = WordItem("п", 0.75)
+
+    print(word_line.compare(find_word1))
+
 
     # print(replace_characters("крф", test_trantab))
     # print(replace_characters("крв", test_trantab))
